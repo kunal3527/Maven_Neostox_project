1a1968aed693c8bed3dcb4be2d879c73af10614b
kunal3527 <kunaladmane2000@gmail.com>
2022-12-23 22:46:30 +0530

    modifide 1 test case & add spaces

--- src/test/java/TestCases/NeoStoxTestclass.java
+++ src/test/java/TestCases/NeoStoxTestclass.java
@@ -85,7 +85,7 @@ public class NeoStoxTestclass extends NeoStoxBase {
 	@Test
 	public void validatebalance() throws IOException, InterruptedException
 	{
-		Assert.fail();
+		
 		Thread.sleep(1000);
 		Assert.assertNotNull(home.getAccBalance(driver), "TC is failed unable to fetch balance");
 	//	CommenUtility.Screenshot(driver, home.getAccBalance(driver));
@@ -99,10 +99,6 @@ public class NeoStoxTestclass extends NeoStoxBase {
 	}
 
 
-	
-	
-	
-	
 	@AfterClass
 	public void closebrowser()
 	{
